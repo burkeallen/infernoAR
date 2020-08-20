1b661f0589ae58c6a0e4876ca2a472f4ff4cb0a4
burkeallen <burkeallen@users.noreply.github.com>
2020-08-20 10:04:21 -0400

    initial test code

---
+++ novo-samples/expo-hall/expo-hall.js
@@ -0,0 +1,69 @@
+var ntarDirectoryCategories = [
+  {name: 'Category 1', filter: 'category1'},
+  {name: 'Category 2', filter: 'category2'},
+  {name: 'Category 3', filter: 'category3'},
+  {name: 'Category 4', filter: 'category4'},
+];
+
+var ntarDirectoryList = [
+  {
+    brand: 'Kazio',
+    category: 'category1',
+    keywords: 'Fully-configurable fault-tolerant groupware',
+    img: 'https://studio304nextechar.freetls.fastly.net/infernoAR/avc/assets/placeholder/sm-booth-01.jpg',
+  },
+  {
+    brand: 'Edgeblab',
+    category: 'category2',
+    keywords: 'Decentralized heuristic paradigm',
+    img: 'https://studio304nextechar.freetls.fastly.net/infernoAR/avc/assets/placeholder/sm-booth-02.jpg',
+  },
+  {
+    brand: 'Youopia',
+    category: 'category3',
+    keywords: 'Decentralized national functionalities',
+    img: 'https://studio304nextechar.freetls.fastly.net/infernoAR/avc/assets/placeholder/sm-booth-03.jpg',
+  },
+  {
+    brand: 'Chatterbridge',
+    category: 'category4',
+    keywords: 'Cross-platform next generation paradigm',
+    img: 'https://studio304nextechar.freetls.fastly.net/infernoAR/avc/assets/placeholder/sm-booth-04.jpg',
+  },
+  {
+    brand: 'Yakitri',
+    category: 'category1',
+    keywords: 'De-engineered systematic capacity',
+    img: 'https://studio304nextechar.freetls.fastly.net/infernoAR/avc/assets/placeholder/sm-booth-01.jpg',
+  },
+  {
+    brand: 'Zoozzy',
+    category: 'category2',
+    keywords: 'Open-source optimizing hierarchy',
+    img: 'https://studio304nextechar.freetls.fastly.net/infernoAR/avc/assets/placeholder/sm-booth-02.jpg',
+  },
+  {
+    brand: 'Oyoloo',
+    category: 'category3',
+    keywords: 'Synergized 5th generation circuit',
+    img: 'https://studio304nextechar.freetls.fastly.net/infernoAR/avc/assets/placeholder/sm-booth-03.jpg',
+  },
+  {
+    brand: 'Cogidoo',
+    category: 'category4',
+    keywords: 'Re-engineered attitude-oriented portal',
+    img: 'https://studio304nextechar.freetls.fastly.net/infernoAR/avc/assets/placeholder/sm-booth-04.jpg',
+  },
+  {
+    brand: 'Kayveo',
+    category: 'category1',
+    keywords: 'Profit-focused dynamic architecture',
+    img: 'https://studio304nextechar.freetls.fastly.net/infernoAR/avc/assets/placeholder/sm-booth-01.jpg',
+  },
+  {
+    brand: 'Ainyx',
+    category: 'category2',
+    keywords: 'Ameliorated 24/7 workforce',
+    img: 'https://studio304nextechar.freetls.fastly.net/infernoAR/avc/assets/placeholder/sm-booth-02.jpg',
+  },
+];
