6847cd1163ae168132374954b1bf2a76139276fe
burkeallen <burkeallen@users.noreply.github.com>
2020-08-21 15:09:16 -0400

    building novo sample

---
+++ novo-samples/expo-burke/expo.js
@@ -0,0 +1,61 @@
+class Expo {
+  constructor() {
+    this.data = {};
+  }
+
+  loadScript(src) {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.onload = resolve;
+      script.onerror = reject;
+      script.src = src;
+      document.head.append(script);
+    });
+  }
+
+  loadStyleSheet(src) {
+    return new Promise((resolve, reject) => {
+      const styleSheet = document.createElement('link');
+      styleSheet.type = 'text/css';
+      styleSheet.rel = 'stylesheet';
+      styleSheet.onload = resolve;
+      styleSheet.onerror = reject;
+      styleSheet.href = src;
+      document.head.append(styleSheet);
+      resolve(true);
+    });
+  }
+
+  fetchData(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(response => response.text())
+        .then(result => {
+          const data = JSON.parse(result);
+          this.data = data;
+          resolve(data);
+        })
+        .catch(err => {
+          console.error('error', err);
+          reject(Error(err));
+        });
+    });
+  }
+
+  init() {
+    Promise.all([
+      this.loadScript('https://unpkg.com/swiper/swiper-bundle.min.js'),
+      this.loadStyleSheet('https://unpkg.com/swiper/swiper-bundle.min.css'),
+      this.loadStyleSheet('expo.css'),
+      this.loadScript('swiper.js'),
+      this.loadStyleSheet('swiper.css'),
+    ])
+      .then(() => {
+        new NovoSwiper(this.data.slides);
+      });
+
+  }
+
+
+}
---
+++ novo-samples/expo-burke/swiper.js
@@ -0,0 +1,51 @@
+class NovoSwiper {
+  constructor(slides) {
+    this.initCarousel(slides);
+  }
+
+  initCarousel(slides) {
+    const carousel = new Swiper('.swiper-container', {
+      autoHeight: true,
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 30,
+      grabCursor: true,
+      preventClicks: false,
+      preventClicksPropagation: false,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    });
+
+    slides.forEach(item => {
+      carousel.appendSlide([this.buildSlide(item.image, item.url)]);
+    });
+  }
+
+  buildSlide(image, url) {
+    return url ? this.buildClickable(image, url) : this.buildStatic(image);
+  }
+
+  buildClickable(image, url) {
+    return '<a class="swiper-slide" href="' + url + '" target="_blank"><img alt="sponsor banner" class="novo-swiper-image" src="' + image + '"></a>';
+  }
+
+  buildStatic(image) {
+    return '<div class="swiper-slide"><img alt="sponsor banner" class="novo-swiper-image" src="' + image + '"></div>';
+  }
+
+}
+
+
+
+
+
