76d376e1427e53ab61b94d836026cb5147cb8bfc
burkeallen <burkeallen@users.noreply.github.com>
2020-09-21 10:27:32 -0400

    updates

--- project-usf/Google Apps Script/novo-utils.js
+++ project-usf/Google Apps Script/novo-utils.js
@@ -5,28 +5,52 @@ class NovoUtils {
   }
 
 
+  /**
+   **  be sure to update the range if you add columns of data to your import sheet
+   **/
   fetchNewUsers() {
-    const newUsers = Sheets.Spreadsheets.Values.get(this.config.sheetId, 'users!A:G').values;
+    const data = Sheets.Spreadsheets.Values.get(this.config.sheetId, 'users!A:J').values;
     //remove the header
-    newUsers.shift();
-
-    return newUsers.map((rowData) => {
-      this.mapUser(rowData)
+    data.shift();
+    //map array to object
+    return data.map((row) => {
+      return this.mapUser(row);
     });
   }
 
-  mapUser(rowData) {
-    // group	first name	last name	org name	description	bio	email address
+  /**
+   **  be sure to update mapping if you add or remove items, or move items(columns) around in the sheet
+   **/
+  mapUser(row) {
+    //Group, Email, First Name, Last Name, University/Business, Position, Bio, Website, LinkedIn, Image
     return {
-      groupName: rowData[0],
-      firstName: rowData[1],
-      lastName: rowData[2],
-      org: rowData[3],
-      desc: rowData[4],
-      bio: rowData[5],
-      email: rowData[6]
+      groupName: row[0],
+      email: row[1],
+      firstName: row[2],
+      lastName: row[3],
+      org: row[4],
+      position: row[5],
+      bio: row[6],
+      website: row[7],
+      linkedIn: row[8],
+      image: row[9]
     }
   }
 
+  listFolderContents(sheet) {
+    sheet.clear();
+    const folderIds = this.config.imageFolderIds.split(',');
+
+    folderIds.forEach((id) => {
+      const folder = DriveApp.getFolderById(id);
+      console.log('listing files in ', folder.getName());
+      const files = folder.getFiles();
+      while(files.hasNext()) {
+        const file = files.next();
+        sheet.appendRow( [file.getName(), file.getId()] );
+      }
+    });
+
+  }
 
 }
