9e2deec05ce48b9cb56f5eb7fd5c34a31df7beac
burkeallen <burkeallen@users.noreply.github.com>
2020-11-12 12:59:58 -0500

    TAG updates

---
+++ tag/menu.js
@@ -0,0 +1,57 @@
+window.localTemplateMenuItems = [{
+  "file": "category/agenda.html",
+  "snippetID": "06598932-4048-447a-800f-50c692318d81",
+  "name": "Agenda"
+}, {
+  "file": "category/attendees-list.html",
+  "snippetID": "f32934c2-47c6-46f3-864c-c6b7420dc9e1",
+  "name": "Attendees List"
+}, {
+  "file": "category/exploration-challenge.html",
+  "snippetID": "54697103-85d0-4bb6-9730-fc787c594567",
+  "name": "Exploration Challenge"
+}, {
+  "file": "category/expo-hall.html",
+  "snippetID": "d16e2b2e-3909-421c-a0bb-9c14f1132ec9",
+  "name": "Expo Hall"
+}, {
+  "file": "category/home.html",
+  "snippetID": "9dfe9977-49f4-43a4-9cb4-564b61e43801",
+  "name": "Home"
+}, {
+  "file": "category/library.html",
+  "snippetID": "a7f4b31e-c8e7-4760-af59-25d8c3e596de",
+  "name": "Library"
+}, {
+  "file": "category/meet-the-speakers.html",
+  "snippetID": "f32934c2-47c6-46f3-864c-c6b7420dc9e1",
+  "name": "Meet The Speakers"
+}, {
+  "file": "category/public.html",
+  "snippetID": "f124ad58-3705-4328-ae05-5361f85970d3",
+  "name": "Public"
+}, {
+  "file": "category/support.html",
+  "snippetID": "4701678e-f8c5-43c1-9d81-09bc7b073a90",
+  "name": "Support"
+}, {
+  "file": "login/login.html",
+  "snippetID": "00437f1d-1e1c-4ad8-ad8b-a47a33c54110",
+  "name": "Login"
+}, {
+  "file": "player/breakout-room-1.html",
+  "snippetID": "56a867a1-b796-4bde-aa92-005359048f39",
+  "name": "Breakout Room 1"
+}, {
+  "file": "player/breakout-room-2.html",
+  "snippetID": "04293f06-a45f-4db1-b7d0-6caf74160ec5",
+  "name": "Breakout Room 2"
+}, {
+  "file": "player/main-stage.html",
+  "snippetID": "aa7edb03-ddb4-4001-8a42-a26283149017",
+  "name": "Main Stage"
+}, {
+  "file": "reg/registration-group.html",
+  "snippetID": "eddfd2c3-0fa0-4ef2-8966-f7f2594bad3d",
+  "name": "Registration Group"
+}]
