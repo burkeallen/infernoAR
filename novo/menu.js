cb4c9a2da2799d8c2b7d1dc162a6f9fb83554bc9
burkeallen <burkeallen@users.noreply.github.com>
2020-12-01 10:20:55 -0500

    Added TedxMalmo Project

--- novo/menu.js
+++ novo/menu.js
@@ -1,1 +1,121 @@
-window.localTemplateMenuItems = [{"file":"CategoryLandingPage/booth-2.html","snippetID":"bf118c91-38f6-43cc-b0f8-b3ae6bb05f0e","name":"Booth 2"},{"file":"CategoryLandingPage/booth.html","snippetID":"985bc330-a712-4c60-b534-7460c592bb11","name":"Booth"},{"file":"CategoryLandingPage/category-landing-page-sample.html","snippetID":"4fad2314-d04f-429e-8d99-b38e2e227cd5","name":"Category Landing Page Sample"},{"file":"CategoryLandingPage/chrispg.html","snippetID":"c93a8cb7-3daa-4c7c-9970-992e4b78fab6","name":"Chrispg"},{"file":"CategoryLandingPage/delete-me.html","snippetID":"4f1f3f19-af9d-4030-b2e7-af942890e3bb","name":"Delete Me"},{"file":"CategoryLandingPage/exhibition-hall.html","snippetID":"50371715-4fcf-43c1-8763-db8125902cf9","name":"Exhibition Hall"},{"file":"CategoryLandingPage/gatherly-testing.html","snippetID":"af6bb600-e8a1-41f6-9c13-a712bbd47d54","name":"Gatherly Testing"},{"file":"CategoryLandingPage/inferno-cli-test.html","snippetID":"7cf76e30-c362-4aa9-9662-e912157b3094","name":"Inferno Cli Test"},{"file":"CategoryLandingPage/landing-page-example.html","snippetID":"1004ffeb-31be-4cbd-a58b-414392822e44","name":"Landing Page Example"},{"file":"CategoryLandingPage/liquid-test-with-user-images.html","snippetID":"faab27f5-866c-4af5-b472-69ba6c5a85ea","name":"Liquid Test With User Images"},{"file":"CategoryLandingPage/lobby.html","snippetID":"1f09670c-e792-4125-98f2-dfef35411b1e","name":"Lobby"},{"file":"CategoryLandingPage/swm-mainstage-redirect.html","snippetID":"26dd3a3d-ef3a-4c94-9e42-3bc4ef0f77be","name":"Swm Mainstage Redirect"},{"file":"CategoryLandingPage/test-client-lobby.html","snippetID":"aae9dab4-4d21-41d6-ab3d-bab194dd3aae","name":"Test Client Lobby"},{"file":"CategoryLandingPage/testing-code-snippet-category-page.html","snippetID":"82a17f25-5b96-4a30-933c-811098a2f043","name":"Testing Code Snippet Category Page"},{"file":"CategoryLandingPage/testingpg.html","snippetID":"fc0d8f4f-7873-49e6-a14f-46326088b4f7","name":"Testingpg"},{"file":"CategoryLandingPage/usf-auditorium-redirect.html","snippetID":"ed73ee6f-ba7c-4878-8bab-d3b76ea92531","name":"Usf Auditorium Redirect"},{"file":"CategoryLandingPage/usf-main-stage.html","snippetID":"e7d23e9f-55f3-4eb5-adef-281887b9ce73","name":"Usf Main Stage"},{"file":"CategoryLandingPage/uskg-booth-play-and-learn.html","snippetID":"f15e993f-e072-4bb6-a560-ad3f1d349966","name":"Uskg Booth Play And Learn"},{"file":"CategoryLandingPage/uskg-booth-products.html","snippetID":"a0bb99b1-fd78-4f8a-a200-b7a02f87cb48","name":"Uskg Booth Products"},{"file":"CategoryLandingPage/uskg-booth-tournamament.html","snippetID":"fec290bb-0735-4fb1-a9c3-9e15e22c60ae","name":"Uskg Booth Tournamament"},{"file":"CategoryLandingPage/uskg-lobby.html","snippetID":"b77fe8a1-ad70-4cab-b129-ac0b2c9c7fa5","name":"Uskg Lobby"},{"file":"customgroupregistration/test-group-registration.html","snippetID":"ec251dd4-b5ca-4f09-bd7e-dee4873e7215","name":"Test Group Registration"},{"file":"CustomPlayerPage/delete-me.html","snippetID":"98932c37-8f4b-45c8-bfb0-40d86fa3fa3a","name":"Delete Me"},{"file":"CustomPlayerPage/player-page-2.html","snippetID":"2dd95ae6-e1de-4a76-ab7d-1228997abe5d","name":"Player Page 2"},{"file":"CustomPlayerPage/player-page-example.html","snippetID":"8a0851f9-930c-46ea-9e40-a0a00a2f53d5","name":"Player Page Example"},{"file":"CustomPlayerPage/swm-mainstage.html","snippetID":"357c6e6b-7bdc-4b4b-9eae-31ba29863c7b","name":"Swm Mainstage"},{"file":"CustomPlayerPage/trivia-player.html","snippetID":"dee1b9fb-d0fa-4920-bb9f-4bf3aefedafa","name":"Trivia Player"},{"file":"CustomPlayerPage/usf-auditorium.html","snippetID":"42c409f3-2862-4310-8156-096ff1c3a019","name":"Usf Auditorium"},{"file":"emailreminderupcoming/event-upcoming-email-taylor-test.html","snippetID":"9be030ec-cc92-4325-a2c0-3568aff3aac5","name":"Event Upcoming Email Taylor Test"}]
+window.localTemplateMenuItems = [{
+  "file": "booth-3.html",
+  "snippetID": "bf118c91-38f6-43cc-b0f8-b3ae6bb05f0e",
+  "name": "Booth 3"
+}, {
+  "file": "CategoryLandingPage/attendees-list-sample.html",
+  "snippetID": "7cf76e30-c362-4aa9-9662-e912157b3094",
+  "name": "Attendees List Sample"
+}, {
+  "file": "CategoryLandingPage/booth-2.html",
+  "snippetID": "bf118c91-38f6-43cc-b0f8-b3ae6bb05f0e",
+  "name": "Booth 2"
+}, {
+  "file": "CategoryLandingPage/booth-3.html",
+  "snippetID": "bf118c91-38f6-43cc-b0f8-b3ae6bb05f0e",
+  "name": "Booth 3"
+}, {
+  "file": "CategoryLandingPage/booth.html",
+  "snippetID": "985bc330-a712-4c60-b534-7460c592bb11",
+  "name": "Booth"
+}, {
+  "file": "CategoryLandingPage/category-landing-page-sample.html",
+  "snippetID": "4fad2314-d04f-429e-8d99-b38e2e227cd5",
+  "name": "Category Landing Page Sample"
+}, {
+  "file": "CategoryLandingPage/chrispg.html",
+  "snippetID": "c93a8cb7-3daa-4c7c-9970-992e4b78fab6",
+  "name": "Chrispg"
+}, {
+  "file": "CategoryLandingPage/delete-me.html",
+  "snippetID": "4f1f3f19-af9d-4030-b2e7-af942890e3bb",
+  "name": "Delete Me"
+}, {
+  "file": "CategoryLandingPage/exhibition-hall.html",
+  "snippetID": "50371715-4fcf-43c1-8763-db8125902cf9",
+  "name": "Exhibition Hall"
+}, {
+  "file": "CategoryLandingPage/gatherly-testing.html",
+  "snippetID": "af6bb600-e8a1-41f6-9c13-a712bbd47d54",
+  "name": "Gatherly Testing"
+}, {
+  "file": "CategoryLandingPage/landing-page-example.html",
+  "snippetID": "1004ffeb-31be-4cbd-a58b-414392822e44",
+  "name": "Landing Page Example"
+}, {
+  "file": "CategoryLandingPage/liquid-test-with-user-images.html",
+  "snippetID": "faab27f5-866c-4af5-b472-69ba6c5a85ea",
+  "name": "Liquid Test With User Images"
+}, {
+  "file": "CategoryLandingPage/lobby.html",
+  "snippetID": "1f09670c-e792-4125-98f2-dfef35411b1e",
+  "name": "Lobby"
+}, {
+  "file": "CategoryLandingPage/test-client-lobby.html",
+  "snippetID": "aae9dab4-4d21-41d6-ab3d-bab194dd3aae",
+  "name": "Test Client Lobby"
+}, {
+  "file": "CategoryLandingPage/testing-code-snippet-category-page.html",
+  "snippetID": "82a17f25-5b96-4a30-933c-811098a2f043",
+  "name": "Testing Code Snippet Category Page"
+}, {
+  "file": "CategoryLandingPage/testingpg.html",
+  "snippetID": "fc0d8f4f-7873-49e6-a14f-46326088b4f7",
+  "name": "Testingpg"
+}, {
+  "file": "CategoryLandingPage/usf-auditorium-redirect.html",
+  "snippetID": "ed73ee6f-ba7c-4878-8bab-d3b76ea92531",
+  "name": "Usf Auditorium Redirect"
+}, {
+  "file": "CategoryLandingPage/usf-main-stage.html",
+  "snippetID": "e7d23e9f-55f3-4eb5-adef-281887b9ce73",
+  "name": "Usf Main Stage"
+}, {
+  "file": "CategoryLandingPage/uskg-booth-play-and-learn.html",
+  "snippetID": "f15e993f-e072-4bb6-a560-ad3f1d349966",
+  "name": "Uskg Booth Play And Learn"
+}, {
+  "file": "CategoryLandingPage/uskg-booth-products.html",
+  "snippetID": "a0bb99b1-fd78-4f8a-a200-b7a02f87cb48",
+  "name": "Uskg Booth Products"
+}, {
+  "file": "CategoryLandingPage/uskg-booth-tournamament.html",
+  "snippetID": "fec290bb-0735-4fb1-a9c3-9e15e22c60ae",
+  "name": "Uskg Booth Tournamament"
+}, {
+  "file": "CategoryLandingPage/uskg-lobby.html",
+  "snippetID": "b77fe8a1-ad70-4cab-b129-ac0b2c9c7fa5",
+  "name": "Uskg Lobby"
+}, {
+  "file": "customgroupregistration/test-group-registration.html",
+  "snippetID": "ec251dd4-b5ca-4f09-bd7e-dee4873e7215",
+  "name": "Test Group Registration"
+}, {
+  "file": "CustomPlayerPage/delete-me.html",
+  "snippetID": "98932c37-8f4b-45c8-bfb0-40d86fa3fa3a",
+  "name": "Delete Me"
+}, {
+  "file": "CustomPlayerPage/player-page-2.html",
+  "snippetID": "2dd95ae6-e1de-4a76-ab7d-1228997abe5d",
+  "name": "Player Page 2"
+}, {
+  "file": "CustomPlayerPage/player-page-example.html",
+  "snippetID": "8a0851f9-930c-46ea-9e40-a0a00a2f53d5",
+  "name": "Player Page Example"
+}, {
+  "file": "CustomPlayerPage/swm-mainstage.html",
+  "snippetID": "357c6e6b-7bdc-4b4b-9eae-31ba29863c7b",
+  "name": "Swm Mainstage"
+}, {
+  "file": "CustomPlayerPage/trivia-player.html",
+  "snippetID": "dee1b9fb-d0fa-4920-bb9f-4bf3aefedafa",
+  "name": "Trivia Player"
+}, {
+  "file": "CustomPlayerPage/usf-auditorium.html",
+  "snippetID": "42c409f3-2862-4310-8156-096ff1c3a019",
+  "name": "Usf Auditorium"
+}, {
+  "file": "emailreminderupcoming/event-upcoming-email-taylor-test.html",
+  "snippetID": "9be030ec-cc92-4325-a2c0-3568aff3aac5",
+  "name": "Event Upcoming Email Taylor Test"
+}]
